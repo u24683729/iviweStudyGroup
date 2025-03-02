ce8bb4f2b07a04d3f2f2ccaa81541f5b2beeb014
u2468 <u2468@MISSVIKWAA>
2025-03-03 00:27:48 +0200

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iviweStudyGroup.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult StudyGroup()
+        {
+            List<Models.Student> people = new List<Models.Student>();
+            people.Add(new Models.Student { Std_ID = 24683729, Std_Name = "Iviwe", Std_Surname = "Vikwa", Std_Email = "u24683729@tuks.co.za" });
+            people.Add(new Models.Student { Std_ID = 23784718, Std_Name = "Thina", Std_Surname = "Magqira", Std_Email = "u23784718@tuks.co.za" });
+            people.Add(new Models.Student { Std_ID = 24979962, Std_Name = "Sivuyisiwe", Std_Surname = "Ngalo", Std_Email = "u24979962@tuks.co.za" });
+            people.Add(new Models.Student { Std_ID = 23641925, Std_Name = "Nkhensani", Std_Surname = "Notigo", Std_Email = "u23641925@tuks.co.za" });
+            people.Add(new Models.Student { Std_ID = 24885062, Std_Name = "Tumisho", Std_Surname = "Matlala", Std_Email = "u24885062@tuks.co.za" });
+            return View(people);
+        }
+
+    }
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace iviweStudyGroup.Models
+{
+    public class Student
+    {
+
+        [Display(Name = "STUDENT NUMBER")]
+        public int Std_ID { get; set; }
+
+        [Display(Name = "NAME")]
+        public string Std_Name { get; set; }
+
+        [Display(Name = "SURNAME")]
+        public string Std_Surname { get; set; }
+
+        [Display(Name = "EMAIL ADDRESS")]
+        public string Std_Email { get; set; }
+    }
+
+}
